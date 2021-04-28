1a495c8b4911ca571ef0fd9716ae8b7437768ab5
veins <veins@instant-veins.car2x.org>
2021-04-28 15:42:30 +0000

    clone repo tutorial

--- ProgettoNetSec/out/gcc-release/src/veins_inet/VeinsInetSampleMessage_m.h.d
+++
@@ -1,9 +0,0 @@
-veins_inet/VeinsInetSampleMessage_m.cc veins_inet/VeinsInetSampleMessage_m.h : \
-	veins_inet/VeinsInetSampleMessage.msg \
-	C:/OMNET/omnetpp-5.6.2/workspace/inet/src\inet/common/INETDefs.msg \
-	C:/OMNET/omnetpp-5.6.2/workspace/inet/src\inet/common/Units.msg \
-	C:/OMNET/omnetpp-5.6.2/workspace/inet/src\inet/common/packet/chunk/Chunk.msg
-veins_inet/VeinsInetSampleMessage.msg:
-C:/OMNET/omnetpp-5.6.2/workspace/inet/src\inet/common/INETDefs.msg:
-C:/OMNET/omnetpp-5.6.2/workspace/inet/src\inet/common/Units.msg:
-C:/OMNET/omnetpp-5.6.2/workspace/inet/src\inet/common/packet/chunk/Chunk.msg:
